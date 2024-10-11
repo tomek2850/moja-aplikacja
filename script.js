ec746441808f0ffc7b8661e364e0f1f7d99c58c1
tomek2850 <tomek2850@gmail.com>
2024-10-11 16:53:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ newRow.appendChild(newCell);
 tableBody.appendChild(newRow);
 saveToLocalStorage(); // Zapisz do LocalStorage
 });
+
 // Funkcja usuwająca najnowszy wpis
 document.getElementById('deleteTimeBtn').addEventListener('click', function() {
 const tableBody = document.getElementById('tableBody');
@@ -30,15 +31,17 @@ tableBody.deleteRow(tableBody.rows.length - 1); // Usunięcie ostatniego wiersza
 saveToLocalStorage(); // Zapisz do LocalStorage
 }
 });
-// Funkcja zapisująca do LocalStorage function
-saveToLocalStorage() {
+
+// Funkcja zapisująca do LocalStorage
+function saveToLocalStorage() {
 const tableBody = document.getElementById('tableBody');
 const times = [];
 for (let i = 0; i < tableBody.rows.length; i++) {
-times.push(tableBody.rows[i].cells[0].textContent);
+    times.push(tableBody.rows[i].cells[0].textContent);
+    }
+    localStorage.setItem(tabId + '_savedTimes', JSON.stringify(times)); // Użyj unikalnego klucza
 }
-localStorage.setItem(tabId + '_savedTimes', JSON.stringify(times)); // Użyj unikalnego klucza
-}
+
 // Funkcja ładująca dane z LocalStorage
 function loadFromLocalStorage() {
 const savedTimes = JSON.parse(localStorage.getItem(tabId + '_savedTimes')) || [];
@@ -48,26 +51,30 @@ const tableBody = document.getElementById('tableBody');
 tableBody.innerHTML = '';
 
 savedTimes.forEach(time => {
-const newRow = document.createElement('tr');
-const newCell = document.createElement('td');
-newCell.textContent = time;
-newRow.appendChild(newCell);
-tableBody.appendChild(newRow);
-});
+    const newRow = document.createElement('tr');
+    const newCell = document.createElement('td');
+    newCell.textContent = time;
+    newRow.appendChild(newCell);
+    tableBody.appendChild(newRow);
+    });
 }
+
 // Funkcja zapisująca wybór dropdownu do LocalStorage
 function saveDropdownSelection() {
-const selectedValue = document.getElementById('options').value;
-localStorage.setItem(tabId + '_selectedOption', selectedValue); // Użyj unikalnego klucza
+    const selectedValue = document.getElementById('options').value;
+    localStorage.setItem(tabId + '_selectedOption', selectedValue); // Użyj unikalnego klucza
 }
+
 // Funkcja ładująca wybór dropdownu z LocalStorage
 function loadDropdownSelection() {
 const selectedValue = localStorage.getItem(tabId + '_selectedOption');
-if (selectedValue) { document.getElementById('options').value = selectedValue;
+if (selectedValue) {
+        document.getElementById('options').value = selectedValue;
+        }
 }
-}
+
 // Załaduj dane z LocalStorage przy starcie strony
 window.addEventListener('load', function() {
-loadFromLocalStorage();
-loadDropdownSelection();
+    loadFromLocalStorage();
+    loadDropdownSelection();
 });
